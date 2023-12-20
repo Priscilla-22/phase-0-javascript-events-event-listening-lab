4a15ebdcca2ec46b6ea7d4ae737b5cd5fa2468b6
Priscilla-22 <priscillamw123@gmail.com>
2023-12-20 13:47:09 +0300

    Maintained the original code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// function addingEventListener() {
-
-// const input = document.getElementById('button');
-// function addEventListener() {
-//   alert('I was clicked!');
-// }
-
-// input.addEventListener('click', addEventListener);
-// }
-// addingEventListener();
-
-//create alert using arrow function
 function addingEventListener() {
-  document.getElementById('button').addEventListener('click', () => alert('I was clicked!'))
+
+const input = document.getElementById('button');
+function addEventListener() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', addEventListener);
 }
 addingEventListener();
+
+//create alert using arrow function
+/*function addingEventListener() {
+  document.getElementById('button').addEventListener('click', () => alert('I was clicked!'))
+}
+addingEventListener();*/
