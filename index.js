eb73eede40f61f76b3d4f079f1448981e9376906
Priscilla-22 <priscillamw123@gmail.com>
2023-12-20 13:43:00 +0300

    modified the code to using arrow function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+// function addingEventListener() {
+
+// const input = document.getElementById('button');
+// function addEventListener() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', addEventListener);
+// }
+// addingEventListener();
+
+//create alert using arrow function
 function addingEventListener() {
-
-const input = document.getElementById('button');
-function addEventListener() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', addEventListener);
+  document.getElementById('button').addEventListener('click', () => alert('I was clicked!'))
 }
 addingEventListener();
